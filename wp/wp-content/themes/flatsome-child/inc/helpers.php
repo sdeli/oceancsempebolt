07afeeb6ca3b3eaa1d1afd3c9ffd5e9b2db84662
sdeli <bgfkszmsdeli@gmail.com>
2022-05-17 13:58:52 +0200

    feat/pluginify-gtm

--- wp/wp-content/themes/flatsome-child/inc/helpers.php
+++ wp/wp-content/themes/flatsome-child/inc/helpers.php
@@ -3,42 +3,6 @@
 /**
  * @return array<string>
  */
-// function get_attributes_by_categories(): array {
-//   global $wpdb;
-//   $result = $wpdb->get_results ("SELECT * FROM  view_colors_by_categories");
-//   $colors_by_categories = [];
-//   $i = 0;
-//   foreach ($result as $row) {
-//     echo $i.' foreach2 ======<br>';
-//     var_dump($row);
-//     $currentCategorySlug = $row->category_slug;
-//     $id = $row->color_id;
-//     $slug = $row->color_slug;
-//     $displayName = $row->category_slug;
-//     echo '<br>'.$currentCategorySlug.' prev2 ======<br>';
-//     echo '<br>id '.$colors_by_categories[$currentCategorySlug][$i - 1]->$id.'<br>';
-//     echo '<br>name '.$colors_by_categories[$currentCategorySlug][$i - 1]->$displayName.'<br>';
-//     echo '<br>slug '.$colors_by_categories[$currentCategorySlug][$i - 1]->$slug.'<br>';
-//     echo '<br>1 =======<br>';
-//     var_dump($colors_by_categories);
-//     $colorTemplateValue = new ColorTemplateValues($slug, $id, $displayName, $i);
-//     if ($colors_by_categories[$currentCategorySlug]) {
-//       $colors_by_categories[$currentCategorySlug][] = $colorTemplateValue;
-//     } else {
-//       $colors_by_categories[$currentCategorySlug] = [$colorTemplateValue];
-//     }
-//     echo '<br>2 =======<br>';
-//     var_dump($colors_by_categories);
-//     echo '<br>'.$currentCategorySlug.' now 2 ======<br>';
-//     echo '<br>id '.$colors_by_categories[$currentCategorySlug][$i]->$id.'<br>';
-//     echo '<br>name '.$colors_by_categories[$currentCategorySlug][$i]->$displayName.'<br>';
-//     echo '<br>slug '.$colors_by_categories[$currentCategorySlug][$i]->$slug.'<br>';
-//     $i++;
-//   }
-//   return $colors_by_categories;
-// }
-
-$C = 'constant';
 
 function get_attributes_by_product_categories(): array {
   global $wpdb;
@@ -66,28 +30,6 @@ function get_attributes_by_product_categories(): array {
   }
   
   return $attributes_by_categories;
-}
-
-function get_pallet_shipping_notes($order) {
-  global $C;
-  $randTelNumber = TEL_NUMBERS[array_rand(TEL_NUMBERS)][1];
-
-  return <<<EOD
-Kérjük várd meg kollégánk jelentkezését, aki tájékoztatni fog a szállítás dijáról, valamint időtartamról. Addig is hivd bizalommal kollégánkat a következő telefonszámon: <strong>{$randTelNumber}</strong><br>
-Miután tájékoztatotást kaptál a szállítási díjról es időről, már átutalhatod nekünk a végösszeget, erre a bankszámlaszámra: <strong>{$C('COMPANY_BANK_ACCOUNT')}</strong><br>
-Arra kérünk, hogy rendelésed számát (<strong>{$order->id}</strong>) átutaláskor tüntesd fel a megjegyzés mezőben, köszönjük.
-EOD;
-}
-
-function get_box_shipping_notes($order) {
-  global $C;
-  $randTelNumber = TEL_NUMBERS[array_rand(TEL_NUMBERS)][1];
-
-  return <<<EOD
-  Ahoz hogy elindíthassuk megrendelésed folyamatát, kérjük utald el a végösszeget (<strong>{$order->calculate_totals()}Ft</strong>) erre a bankszámlaszámra: <strong>{$C('COMPANY_BANK_ACCOUNT')}</strong><br>
-  Arra kérünk, hogy rendelésed számát (<strong>{$order->id}</strong>) átutaláskor tüntesd fel a megjegyzés mezőben, köszönjük.<br>
-  Ha bármi kérdésed merülne fel, hivd bizalommal kollégánkat a következő telefonszámon: <strong>{$randTelNumber}</strong>
-EOD;
 }
 
 function get_payment_and_shipment_notes() {
