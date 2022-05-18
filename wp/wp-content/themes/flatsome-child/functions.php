2a9aa227d31962c795ae207a52461a071b45be01
sdeli <bgfkszmsdeli@gmail.com>
2022-05-18 22:58:46 +0200

    feat/product-category-page-title

--- wp/wp-content/themes/flatsome-child/functions.php
+++ wp/wp-content/themes/flatsome-child/functions.php
@@ -4,7 +4,7 @@ require_once(get_theme_file_path('/inc/constants.php'));
 if (wp_get_environment_type() === ENVIRONMENT_TYPE_LOCAL) {
   require_once(get_theme_file_path('/vendor/autoload.php'));
 }
+
 require_once(get_theme_file_path('/inc/classes.php'));
 require_once(get_theme_file_path('/inc/elements.php'));
 require_once(get_theme_file_path('/inc/helpers.php'));
-require_once(get_theme_file_path('/inc/woocommerce/child-theme-structure-wc-category-page-header.php'));
